352d640eefdce18c89ca2fd1c36e97877a241ab3
Travis CI <travis@travis-ci.org>
2017-07-11 18:23:36 -0400

    sock communication roughly working. Shutdown is not graceful.

---
+++ smp.py
@@ -0,0 +1,37 @@
+# my simple message protocol
+import logging
+import socket
+
+BUFFER_SIZE = 1024  # not more than 4 digits
+
+def snd(s, msg) :
+    # msg should be string. 
+    #It gets prepended with the 4 digit length (for rcv) and convert to byte, eg b"ok"
+    msg  = (str(len(msg)).zfill(4) + msg).encode()
+    tot = len(msg)
+    #logging.debug('sending ' + msg.decode() + ' length ' + str(tot))
+    t	= 0
+    while t < tot:
+       #logging.debug('msg ' + msg[t:].decode())
+       sent = s.send(msg[t:])
+       #logging.debug('sent ' + str(sent))
+       if sent == 0 :
+          raise RuntimeError("socket connection broken")
+       t  +=  sent
+    return t
+
+def rcv(s):
+    chunks  = []
+    tot = int(s.recv(4).decode())
+    #logging.debug('rcv tot ' + str(tot))
+    t = 0
+    while t < tot :
+       chunk = s.recv(min(tot, BUFFER_SIZE))
+       #logging.debug('chunk ' + str(chunk))
+       if chunk == b'' : 
+          raise RuntimeError("socket connection broken")
+       chunks.append(chunk)
+       t = t + len(chunk)
+    r = (b''.join(chunks)).decode()
+    #logging.debug('returning chunks ' + r)
+    return  r
