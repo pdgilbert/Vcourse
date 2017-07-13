6162aff09f27a89ac79a148d94c9d7d83dc141cf
Travis CI <travis@travis-ci.org>
2017-07-13 12:00:19 -0400

    added try around connection to make robust in case wifi connection is not in range.

--- distribution.py
+++ distribution.py
@@ -3,6 +3,7 @@
 import socket
 import logging
 import json
+import time
 
 import smp
 
@@ -12,43 +13,51 @@ def distributionCheck(update, shutdown, interval, RC_IP, RC_PORT, BT_ID):
     #     or 'none' indicating no course set yet, or with updated raceObj.
     # BT confirms receipt of update with bt# or hostname?  
     
-    logging.debug('distributionCheck starting')
+    print('distributionCheck starting')
 
     while True:
-        if shutdown.wait(interval):    # effectively sleep too
-           logging.debug('shutting down distributionCheck thread.')
+        if shutdown.wait(0.1):    # effectively sleep too
+           print('shutting down distributionCheck thread.')
            return() 
         
-        # check RC for update
+        # check RC for update. 
+        # Wrapped in try for case when connection fails (wifi out of range).
         #logging.debug('check RC for update.')
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((RC_IP, RC_PORT))
-        
+
         try :
-           with open("BTraceObj.json","r") as f:  raceObj = json.load(f)
-           cid = raceObj['cid']
+    	    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    	    s.connect((RC_IP, RC_PORT))
+    	    
+    	    try :
+    	       with open("BTraceObj.json","r") as f:  raceObj = json.load(f)
+    	       cid = raceObj['cid']
+    	    except :
+    	       cid = 'none'
+    	    
+    	    l = smp.snd(s, cid)
+    	    #logging.debug("BT cid " + str(cid))
+    	    
+    	    r = smp.rcv(s) 
+    	    #logging.debug('r ' + str(r))
+
+    	    if not (r in ('ok', 'none')) :
+                logging.debug('got new raceObj. Writing to file BTraceObj.json')
+                # Next could be a message to zoneSignal thread, but having a
+                # file means the gadget can recover after reboot without
+                # a connection to RC, so write string r to a file
+                with open("BTraceObj.json","w") as f: f.write(r) 
+                update.set()
+
+                l = smp.snd(s, BT_ID)
+                logging.debug("sent  receipt BT " + str(BT_ID))
+
+    	    s.close()
+
         except :
-           cid = 'none'
-        
-        l = smp.snd(s, cid)
-        #logging.debug("BT cid " + str(cid))
-        
-        r = smp.rcv(s) 
-        #logging.debug('r ' + str(r))
+           #logging.debug('no connection.')
+           time.sleep(1)
 
-        if not (r in ('ok', 'none')) :
-            logging.debug('got new raceObj. Writing to file BTraceObj.json')
-            # Next could be a message to zoneSignal thread, but having a
-            # file means the gadget can recover after reboot without
-            # a connection to RC, so write string r to a file
-            with open("BTraceObj.json","w") as f: f.write(r) 
-            update.set()
+        time.sleep(interval)
 
-            l = smp.snd(s, BT_ID)
-            logging.debug("sent  receipt BT " + str(BT_ID))
-
-        s.close()
-        #logging.debug('connection closed')
-
-    logging.debug('distributionCheck should not be here.')
+    raise Exception('distributionCheck should not be here.')
 
