f57fa6250092aaea32c9c1bb591ef8c6def42f94
Paul Gilbert <pdgilbert@users.noreply.github.com>
2017-07-22 13:17:22 -0400

    Big change (should have branched): many things were broken by moving RC distribution into lib/ modules as globals no longer worked, but that was not immediately noticed. Now introduced distribution class and methods so that raceObj is internal to the class and no longer global. It seems to be working, but rough and not well tested.

--- lib/distribution.py
+++ lib/distribution.py
@@ -13,6 +13,8 @@ import threading
 
 import smp
 
+logging.basicConfig(level=logging.DEBUG, format='(%(threadName)-9s) %(message)s',)
+
 def distributionCheck(update, shutdown, interval, RC_IP, RC_PORT, BT_ID):
     # this function is used by BT
 
@@ -25,7 +27,7 @@ def distributionCheck(update, shutdown, interval, RC_IP, RC_PORT, BT_ID):
         
         # check RC for update. 
         # Wrapped in try for case when connection fails (wifi out of range).
-        #logging.debug('check RC for update.')
+        logging.debug('BT check with RC for update.')
 
         try :
     	    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -38,10 +40,10 @@ def distributionCheck(update, shutdown, interval, RC_IP, RC_PORT, BT_ID):
     	       cid = 'none'
     	    
     	    l = smp.snd(s, cid)
-    	    #logging.debug("BT cid " + str(cid))
+    	    logging.debug("BT cid " + str(cid))
     	    
     	    r = smp.rcv(s) 
-    	    #logging.debug('r ' + str(r))
+    	    logging.debug('r ' + str(r))
 
     	    if not (r in ('ok', 'none')) :
                 #logging.debug('got new raceObj. Writing to file BTraceObj.json')
@@ -52,12 +54,12 @@ def distributionCheck(update, shutdown, interval, RC_IP, RC_PORT, BT_ID):
                 update.set()
 
                 l = smp.snd(s, BT_ID)
-                #logging.debug("sent  receipt BT " + str(BT_ID))
+                logging.debug("sent  receipt BT " + str(BT_ID))
 
     	    s.close()
 
         except :
-           #logging.debug('no connection.')
+           logging.debug('no connection.')
            time.sleep(1)
 
         time.sleep(interval)
@@ -67,34 +69,59 @@ def distributionCheck(update, shutdown, interval, RC_IP, RC_PORT, BT_ID):
 
 ####### following are used by RC #######
 
+def distributer(dist):
+   # wait for a connection from a BT and pass it to BThandlerThread
+   logging.debug('distributer starting. Using RC_IP ' + str(dist.RC_IP) + ':' +str(dist.RC_PORT))
+   # wait for connections from BTs and pass each to a BThandlerThread.
+   logging.debug('distributer started thread? ')
+   
+   
+   tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+   tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+   tcpsock.bind((dist.RC_IP, dist.RC_PORT))
+
+   while True:
+       # often not getting shutdown signal. Seems to work better when a BT has connected. FIX
+       if dist.shutdown.wait(0.01): # blocking for interval !! 
+          logging.debug('shutting down distributionHandler thread.')
+          return() 
+       tcpsock.listen(5)  # STALLS HERE LISTENING WITH NO CONNECTIONS, SO NEVER LOOPS  TO shutdown.
+       logging.debug("Waiting for incoming connections on "  + dist.RC_IP + ":" + str(dist.RC_PORT) + " ...")
+       (conn, (ip,port)) = tcpsock.accept()
+       logging.debug('Got connection from ' + ip + ':' + str(port))
+       BThandlerThread(ip,port,conn, dist).start()
+
+   logging.debug('distributionHandler should not be here.')
+
 class BThandlerThread(threading.Thread):
    # handle a connection from a BT. Check if BT is current and update if not.
+   # NB BThandlerThread needs a dist object, not just raceObj, 
+   # because it uses dist.received() te record back to "global" dist.
 
-   def __init__(self,ip,port,sock):
+   def __init__(self,ip,port,sock, dist):
        threading.Thread.__init__(self)
        self.ip = ip
        self.port = port
        self.sock = sock
-       #logging.debug(" New thread started for "+ip+":"+str(port))
+       self.dist = dist
+       logging.debug(" New thread started for "+ip+":"+str(port))
    
    def run(self):
        import smp
        import json
-       
-       global raceObjReceived
- 
+        
        #course id that RC has
-       if (raceObj == None ) : 
+       if (self.dist.raceObj == None ) : 
           RCcid = 'none'
        else :
-          RCcid = raceObj['cid']
+          RCcid = self.dist.raceObj['cid']
        
        BTcid = smp.rcv(self.sock)  #course id that BT has
        
-       #logging.debug(" BTcid " + str(BTcid))
-       #logging.debug(" RCcid " + str(RCcid))
+       logging.debug(" BTcid " + str(BTcid))
+       logging.debug(" RCcid " + str(RCcid))
 
-       if (raceObj == None ) : 
+       if (self.dist.raceObj == None ) : 
              smp.snd(self.sock, 'none')
              #logging.debug('sent none.')
 
@@ -103,36 +130,76 @@ class BThandlerThread(threading.Thread):
              #logging.debug('sent ok.')
              
        else :
-             #logging.debug('sending new raceObj to BT')
-             smp.snd(self.sock, json.dumps(raceObj, indent=4))
+             logging.debug('sending new raceObj to BT')
+             smp.snd(self.sock, json.dumps(self.dist.raceObj, indent=4))
              #logging.debug('new raceObj sent:')
-             r = smp.rcv(self.sock) 
-             logging.debug('ADD BT_ID TO LIST. ' + r)
-             raceObjReceived[r] = time.strftime('%Y-%m-%d %H:%M:%S %Z')
+             bt = smp.rcv(self.sock) 
+             logging.debug('ADD BT_ID TO LIST. ' + bt)
+             self.dist.received( bt, time.strftime('%Y-%m-%d %H:%M:%S %Z'))
  
 
        self.sock.close()
        #logging.debug('closed socket and closing thread.')
 
+class distribution():
 
-def distributionHandler(shutdown, RC_IP, RC_PORT):
-   # wait for connections from BTs and pass each to a BThandlerThread.
-   logging.debug('distributionHandler starting')
-      
-   tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-   tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-   tcpsock.bind((RC_IP, RC_PORT))
+   def __init__(self, shutdown, RC_IP, RC_PORT, raceObj, raceObjReceived):
+      self.shutdown = shutdown
+      self.RC_IP = RC_IP
+      self.RC_PORT = RC_PORT
+      self.raceObj = raceObj
+      self.raceObjReceived = raceObjReceived
+   
+   def prt(self):
+      print(json.dumps(self.raceObj, indent=4))
+      print('Received by:')
+      print(self.raceObjReceived)
 
-   while True:
-       # often not getting shutdown signal. Seems to work better when a BT has connected. FIX
-       if shutdown.wait(0.01): # blocking for interval !! 
-          logging.debug('shutting down distributionHandler thread.')
-          return() 
-       tcpsock.listen(5)  # STALLS HERE LISTENING WITH NO CONNECTIONS, SO NEVER LOOPS  TO shutdown.
-       #logging.debug("Waiting for incoming connections on "  + RC_IP + ":" + str(RC_PORT) + " ...")
-       (conn, (ip,port)) = tcpsock.accept()
-       #logging.debug('Got connection from ' + ip + ':' + str(port))
-       BThandlerThread(ip,port,conn).start()
+   def newRace(self, newraceObj):
+      # should be called by distribute but for now called by makeRaceObj
+      #  which uses global parameters.
+      self.raceObj.clear()
+      self.raceObj.update(newraceObj)
+      print(json.dumps(self.raceObj, indent=4))
 
-   logging.debug('distributionHandler should not be here.')
+   def received(self, bt, tm):
+      self.raceObjReceived.update({str(bt) : tm})
+   
+   def distribute(self):  #, newraceObj):
+      # This works by creating a new version of RaceObj, the BThandlerThread
+      # compares the cid of this with that sent by BT when they connect.
+      # There is no "signal" to distributionHandler or BTs. It depends on them checking in.
+            
+      ## NEED THIS     FIX
+      #makeRaceObj()  # this also syncs with screen but does no calc().
+      #self.raceObj.clear()
+      #self.raceObj.append(newraceObj)
+      logging.debug(" in distribute printing self.raceObj")
+      print(json.dumps(self.raceObj, indent=4))
+   
+      # clear dict of boats that have update
+      self.raceObjReceived.clear()
+   
+      # write raceObj [NOT and also global object info] to a file, for the record, but distribute is done with raceObj
+      with open('distributedCourses/raceObj' + self.raceObj['cid'] + '.json',"w") as f:
+         f.write("\n")
+         f.write("raceObj:\n")
+         json.dump(self.raceObj, f, indent=4)
+         #Race = { 'fl':fl, 'dc':dc, 'ty':ty, 'cl':cl, 'ax':ax, 'll':ll, 'sw':sw, 
+         #       'RC.lat':RC.lat, 'RC.lon':RC.lon, 'S.lat':S.lat, 'S.lon':S.lon, 'M.lat':M.lat, 'M.lon':M.lon,
+         #       'wn':wn, 'cc':cc, 'tt':tt }
+         #f.write("\n")
+         #f.write("Race:\n")
+         #json.dump(Race, f, indent=4)
 
+   
+   
+   
+   
+   
+   
+   
+   
+ 
+
+   
