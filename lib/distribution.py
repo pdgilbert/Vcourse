1c6245c4a41b7ee129ec4cf9991376a2c8368bd4
Paul Gilbert <pdgilbert@users.noreply.github.com>
2017-11-21 17:52:30 -0500

    ongoing re-org of distributer thread.

--- lib/distribution.py
+++ lib/distribution.py
@@ -31,22 +31,22 @@ def distributionCheck(update, shutdown, interval, RC_IP, RC_PORT, BT_ID):
         logging.debug('BT check with RC for update.')
 
         try :
-    	    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    	    s.connect((RC_IP, RC_PORT))
-    	    
-    	    try :
-    	       with open("BTzoneObj.json","r") as f:  zoneObj = json.load(f)
-    	       cid = zoneObj['cid']
-    	    except :
-    	       cid = 'none'
-    	    
-    	    l = smp.snd(s, cid)
-    	    #logging.debug("BT cid " + str(cid))
-    	    
-    	    r = smp.rcv(s) 
-    	    logging.debug('r ' + str(r))
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((RC_IP, RC_PORT))
+            
+            try :
+               with open("BTzoneObj.json","r") as f:  zoneObj = json.load(f)
+               cid = zoneObj['cid']
+            except :
+               cid = 'none'
+            
+            l = smp.snd(s, cid)
+            #logging.debug("BT cid " + str(cid))
+            
+            r = smp.rcv(s) 
+            logging.debug('r ' + str(r))
 
-    	    if not (r in ('ok', 'none')) :
+            if not (r in ('ok', 'none')) :
                 logging.info('got new zoneObj. Writing to file BTzoneObj.json')
                 # Next could be a message to zoneSignal thread, but having a
                 # file means the gadget can recover after reboot without
@@ -57,7 +57,7 @@ def distributionCheck(update, shutdown, interval, RC_IP, RC_PORT, BT_ID):
                 l = smp.snd(s, BT_ID)
                 logging.debug("sent  receipt BT " + str(BT_ID))
 
-    	    s.close()
+            s.close()
 
         except :
            #logging.debug('no connection.')
@@ -68,63 +68,110 @@ def distributionCheck(update, shutdown, interval, RC_IP, RC_PORT, BT_ID):
     raise Exception('distributionCheck should not be here.')
 
 
-####### following are used by RC #######
+####### following used by RC #######
 
-def distributer(dist):
-   # wait for connections from BTs and pass each to a BThandlerThread.
-   logging.info('distributer starting. Using RC_IP ' + str(dist.RC_IP) + ':' +str(dist.RC_PORT))
+class distributer(threading.Thread):
+   # Wait for connections from BTs and pass each to a BThandlerThread.
+   # Maintain zoneObj.
+   # Maintain list distRecvd of boats that have received a distribution.
    
-   tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-   tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-   tcpsock.bind((dist.RC_IP, dist.RC_PORT))
-   # without timeout the while loop waits indefinitely when no BT connect,
-   # so never checks for shutdown.
-   tcpsock.settimeout(5)
-   logging.debug("Incoming socket timeout " + str(tcpsock.gettimeout()))
+   #RC_IP, RC_PORT could be read here from config rather than passed
+   def __init__(self, RC_IP, RC_PORT, shutdown):
+      threading.Thread.__init__(self)
 
-   while True:
-       if dist.shutdown.wait(0.01): # blocking for interval !! 
-          logging.info('shutting down distributionHandler thread.')
-          return() 
-       try:
-          tcpsock.listen(5)  
-          #logging.debug("Waiting for incoming connections on "  + dist.RC_IP + ":" + str(dist.RC_PORT) + " ...")
-          (conn, (ip,port)) = tcpsock.accept()
-          #logging.debug('Got connection from ' + ip + ':' + str(port))
-          BThandlerThread(ip, port, conn, dist).start()
-       except Exception: 
-          pass
+      self.name='distributer'
+      logging.info('distributer starting. Using RC_IP ' + str(RC_IP) + ':' +str(RC_PORT))
+      
+      self.shutdown=shutdown
+      self.tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      self.tcpsock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+      self.tcpsock.bind((RC_IP, RC_PORT))
+      # without timeout the while loop waits indefinitely when no BT connect,
+      # so never checks for shutdown.
+      self.tcpsock.settimeout(5)
+      #logging.debug("Incoming socket timeout " + str(self.tcpsock.gettimeout()))
+      self.zoneObj = { 'cid' : None }
+      self.distRecvd = {}
+      
+   def run(self):
+      logging.debug("distributer started.")
+      while not  self.shutdown.wait(0.01): # blocking for interval
+         try:
+            #logging.debug("distributer listening for incoming connections ...")
+            self.tcpsock.listen(5)  
+            (sock, (ip,port)) = self.tcpsock.accept()
+            #these only take a second, so not need to pass shutdown signal.
+            BThandlerThread(ip, port, sock, self.zoneObj).start()
+         except Exception: 
+            pass
 
-   raise Exception('distributionHandler should not be here.')
+      logging.info('exiting ' + self.name + ' thread.')
+  
+   def distributionRecvd(self):
+      return self.distRecvd
+   
+   def prt(self):
+      print(json.dumps(self.zoneObj, indent=4))
+      print('Received by:')
+      print(self.distRecvd)
+   
+   def addRecvd(self, bt, tm):  # may need queue or something else for this
+      self.distRecvd.update({str(bt) : tm})
+   
+   def distribute(self, zoneObj):
+      #  IT might BE BETTER IF THE ARG WAS raceObj and makezoneObj was done
+      #    here or in a stadium nodule. But probably want raceObj class 
+      #    with methods in stadiumRC first, and stop using globals.
+      # This works by creating a new version of zoneObj, the BThandlerThread
+      # compares the cid of this with that sent by BTs when they connect.
+      # There is no other "signal" to BThandlerThread or to BTs. 
+      # It depends on BTs checking in.
+      logging.debug('in distributer.distribute(), zoneObj:')
+      logging.debug(str(zoneObj))
+
+      self.zoneObj = zoneObj   
+      self.distRecvd = {}    # clear dict of boats that have update
+            
+      #print(json.dumps(self.zoneObj, indent=4))
+   
+      # write zoneObj to a file, for the record, but distribute is done with zoneObj
+      with open('distributedCourses/zoneObj-' + self.zoneObj['cid'] + '.json',"w") as f:
+         f.write("\n")
+         f.write("zoneObj:\n")
+         json.dump(self.zoneObj, f, indent=4)
+
 
 class BThandlerThread(threading.Thread):
    # handle a connection from a BT. Check if BT is current and update if not.
    # NB BThandlerThread needs a dist object, not just zoneObj, 
-   # because it uses dist.received() te record back to "global" dist.
+   # because it uses dist.addRecvd() te record back to distributer.
 
-   def __init__(self,ip,port,sock, dist):
+   def __init__(self, ip, port, sock, zoneObj):
        threading.Thread.__init__(self)
+       self.name='BThandler'
+       #logging.debug('in BThandlerThread init')
        self.ip = ip
        self.port = port
        self.sock = sock
-       self.dist = dist
-   
-   def run(self):
-       import smp
-       import json
-        
+       self.zoneObj = zoneObj
+   def run(self):        
+       logging.debug('in BThandlerThread.run()')
+       logging.debug(str(self.zoneObj))
+
        #course id that RC has
-       if self.dist.zoneObj is None : 
-          RCcid = 'none'
+       if self.zoneObj is None : 
+             RCcid = 'none'
+       elif self.zoneObj['cid'] is None : 
+             RCcid = 'none'
        else :
-          RCcid = self.dist.zoneObj['cid']
+             RCcid = self.zoneObj['cid']
        
        BTcid = smp.rcv(self.sock)  #course id that BT has
        
-       #logging.debug(" BTcid " + str(BTcid))
-       #logging.debug(" RCcid " + str(RCcid))
+       logging.debug(" BTcid " + str(BTcid))
+       logging.debug(" RCcid " + str(RCcid))
 
-       if self.dist.zoneObj is None : 
+       if self.zoneObj is None : 
              smp.snd(self.sock, 'none')
              #logging.debug('sent none.')
 
@@ -134,62 +181,15 @@ class BThandlerThread(threading.Thread):
              
        else :
              #logging.debug('sending new zoneObj to BT')
-             smp.snd(self.sock, json.dumps(self.dist.zoneObj, indent=4))
+             smp.snd(self.sock, json.dumps(self.zoneObj, indent=4))
              #logging.debug('new zoneObj sent:')
              bt = smp.rcv(self.sock) 
-             #logging.debug('ADD BT_ID TO LIST. ' + bt)
-             self.dist.received( bt, time.strftime('%Y-%m-%d %H:%M:%S %Z'))
+             logging.debug('ADD BT_ID TO LIST. ' + bt)
+             #dr.addRecvd( bt, time.strftime('%Y-%m-%d %H:%M:%S %Z'))
  
-
        self.sock.close()
-       #logging.debug('closed socket and closing thread.')
+       logging.debug('closed socket and exiting thread ' + self.name)
 
-class distribution():
-
-   def __init__(self, shutdown, RC_IP, RC_PORT, zoneObj, zoneObjReceived):
-      self.shutdown = shutdown
-      self.RC_IP = RC_IP
-      self.RC_PORT = RC_PORT
-      self.zoneObj = zoneObj
-      self.zoneObjReceived = zoneObjReceived
-   
-   def prt(self):
-      print(json.dumps(self.zoneObj, indent=4))
-      print('Received by:')
-      print(self.zoneObjReceived)
-
-   def newRace(self, newzoneObj):
-      # should be called by distribute but for now called by makezoneObj
-      #  which uses global parameters.
-      self.zoneObj.clear()
-      self.zoneObj.update(newzoneObj)
-
-   def received(self, bt, tm):
-      self.zoneObjReceived.update({str(bt) : tm})
-   
-   def distribute(self):  #, newzoneObj):
-      # This works by creating a new version of zoneObj, the BThandlerThread
-      # compares the cid of this with that sent by BT when they connect.
-      # There is no "signal" to distributionHandler or BTs. It depends on them checking in.
-            
-      #print(json.dumps(self.zoneObj, indent=4))
-   
-      # clear dict of boats that have update
-      self.zoneObjReceived.clear()
-   
-      # write zoneObj [NOT and also global object info] to a file, for the record, but distribute is done with zoneObj
-      with open('distributedCourses/zoneObj-' + self.zoneObj['cid'] + '.json',"w") as f:
-         f.write("\n")
-         f.write("zoneObj:\n")
-         json.dump(self.zoneObj, f, indent=4)
-         #Race = { 'fl':fl, 'dc':dc, 'ty':ty, 'cl':cl, 'ax':ax, 'll':ll, 'sw':sw, 
-         #       'RC.lat':RC.lat, 'RC.lon':RC.lon, 'S.lat':S.lat, 'S.lon':S.lon, 'M.lat':M.lat, 'M.lon':M.lon,
-         #       'wn':wn, 'cc':cc, 'tt':tt }
-         #f.write("\n")
-         #f.write("Race:\n")
-         #json.dump(Race, f, indent=4)
-
-   
    
    
    
