4c9d7c4a6c27ba11432f8fcacbff244553313daf
Paul Gilbert <pdgilbert@users.noreply.github.com>
2017-12-01 09:48:47 -0500

    added gpsConnection class to gpsPos.py and moved many connection details from stadiumBT and stadiumRC.

--- lib/gpsPos.py
+++ lib/gpsPos.py
@@ -5,15 +5,33 @@ __version__ = '0.0.3'
 
 
 # PUT INTO UNIT TESTING
+#from gpsPos import gpsConnection
 #from gpsPos import gpsPos
-#pt = gpsPos.gpsPos(45.395676, -75.676829)
+#pt = gpsPos(45.395676, -75.676829)
 #pt = gpsPos(100.395676, -75.676829)
 #pt = gpsPos(45.395676, -275.676829)
 #pt2 = gpsPos.move(pt, 240, 1.0)
 #gpsPos.nm(pt, pt2)  
 # if not (1.0 == gpsPos.nm(pt, pt2).distance)  then something is wrong
 
-#  p = gpsPos.getGPS()
+#  con = gpsConnection()
+#  p = con.getGPS()
+#  p.lon
+#  p.lat
+
+#It is possible to use something like gps2net on a phone and connect to it,
+# but python gpsd expects json format data while gps2net supplies raw NMEA0183.
+# Thus it is necessary to run gpsd to talk to the phone and translate:
+#[sudo] gpsd -S 2950 tcp://192.168.2.195:6000  # uses 127.0.0.1 port 2950
+#[sudo] gpsd -S 2950 tcp://10.42.0.101:6000    # uses 127.0.0.1 port 2950
+#[sudo] gpsd -S 2949 tcp://10.42.0.74:6000     # uses 127.0.0.1 port 2949
+
+# but NB: python gpsd supports only one connection at a time (in one session)
+
+#  con = gpsConnection(host="127.0.0.1", port=2950)
+#  p = con.getGPS()
+#  p.lon
+#  p.lat
 
 # gpsPos.heading(gpsPos(44.20978198674271, -76.51016279425306), 
 #                gpsPos(44.20144865340938, -76.53029941039073))  # 240 
@@ -34,20 +52,40 @@ __version__ = '0.0.3'
 #                gpsPos(0, -76.49359110683335))  # 90
 
 # gpsPos.heading(gpsPos(44.21048983959772,  0),
-#                gpsPos(44.198704726577944, 0))  # 1380
+#                gpsPos(44.198704726577944, 0))  # 180
 
 import gpsd
 import math
-from gpsd import get_current
 import logging
 
-def getGPS():
-  """Get the current GPS position and return a gpsPos object."""
-  try:
-     p = get_current()
-     return(gpsPos(p.lat, p.lon, p.time))
-  except:
-     return(None)
+class gpsConnection():
+   def __init__(self, host="127.0.0.1", port=2947):
+      """Define and return a gpsConnection object. (really a fake.)"""
+      # The connection is a global in gpsd, and is simply left in place.
+      # This only works if there is a reconnect each time there is a change,
+      # which is what happens with stadiumRC. To actually work properly the
+      # conncetion has to be part of the object, but that requires changes in gpsd.
+      self.host  = host
+      self.port  = int(port)
+      try:
+         gpsd.connect(host=self.host, port=self.port) 
+      except:
+         logging.info('Cannot connect to GPS at ' + self.host + ':' + str(self.port))
+         # do not kill the thread, it may be temporary
+
+   def getGPS(self):
+      """Get the current GPS position and return a gpsPos object."""
+      try:
+         p = gpsd.get_current()
+      except:
+         try:
+            logging.debug('getGPS attempting reconnect.' )
+            gpsd.connect(host=self.host, port=self.port) # re-connect
+            p = gpsd.get_current()
+         except:
+            return(None)
+      logging.debug('getGPS:' + str(p.lat) + ' ' + str(p.lon) )
+      return(gpsPos(p.lat, p.lon, p.time))
 
 class gpsPos():
     def __init__(self, lat, lon, time=None):
